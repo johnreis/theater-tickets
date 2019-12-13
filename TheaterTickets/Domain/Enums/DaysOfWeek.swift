4b5dc984545ecdaa3ef13bb375fc196575c33e3c
John Lenon Reis <lenooreis@gmail.com>
2019-12-12 21:11:56 -0300

    Added test to children ticket

---
+++ TheaterTickets/Domain/Enums/DaysOfWeek.swift
@@ -0,0 +1,17 @@
+//
+//  DaysOfWeek.swift
+//  TheaterTickets
+//
+//  Created by John Lenon Reis on 12/12/19.
+//  Copyright © 2019 John Lenon Reis. All rights reserved.
+//
+
+enum DaysOfWeek {
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+    case saturday
+    case sunday
+}
--- TheaterTicketsTests/TheaterTicketsTests.swift
+++ TheaterTicketsTests/TheaterTicketsTests.swift
@@ -10,25 +10,18 @@ import XCTest
 @testable import TheaterTickets
 
 class TheaterTicketsTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var ticketCalculator: TicketCalculator
+    
+    func setUp() {
+        self.ticketCalculator = TicketCalculator()
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testShouldCalculateChildrenTicket() {
+        let children = Children()
+        self.ticketCalculator.updateStrategy(with: ChildrenCalculator(client: children))
+        
+        let price = self.priceOfTicket(in .monday)
+        XCTAssertEqual(price, 5.4)
+        
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
